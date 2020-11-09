aa6a3538f5b05fac0a4e2baf60badc2840b78bca
dahailu <ludahai@163.com>
2020-11-09 18:25:36 +0800

    repair folder name bugs

--- libs/Python/utility/PythonModules.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace PythonModules {
-	const static char* RandLANet = "python36.RandLA_Net.inference";
-	const static char* RandLAInterface = "inference";
-	const static char* PYTHON_HOME = "python36";
-}
-
