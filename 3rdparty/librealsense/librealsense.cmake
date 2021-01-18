2ccfc657fd10f8e3c65d004ecd67be9075d9c46d
dahailu <ludahai19@163.com>
2021-01-18 21:18:29 +0800

    fix compiling bugs on windows for assimp

--- 3rdparty/librealsense/librealsense.cmake
+++ 3rdparty/librealsense/librealsense.cmake
@@ -1,6 +1,32 @@
 include(ExternalProject)
 
-if (UNIX OR APPLE)
+if (WIN32)
+	ExternalProject_Add(
+		ext_librealsense
+		PREFIX librealsense
+		GIT_REPOSITORY https://github.com/IntelRealSense/librealsense.git
+		GIT_TAG v2.40.0 # 18 Nov 2020
+		UPDATE_COMMAND ""
+		CMAKE_ARGS
+			-D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+			-D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+			-D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+			-D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+			-D BUILD_SHARED_LIBS=OFF
+			-D BUILD_EXAMPLES=OFF
+			-D BUILD_UNIT_TESTS=OFF
+			-D BUILD_GLSL_EXTENSIONS=OFF
+			-D BUILD_GRAPHICAL_EXAMPLES=OFF
+			-D BUILD_PYTHON_BINDINGS=OFF
+			-D BUILD_WITH_CUDA=${BUILD_CUDA_MODULE}
+			-D FORCE_RSUSB_BACKEND=ON      # https://github.com/IntelRealSense/librealsense/wiki/Release-Notes#release-2400
+			-D USE_EXTERNAL_USB=ON
+			$<$<PLATFORM_ID:Darwin>:-DBUILD_WITH_OPENMP=OFF>
+			$<$<PLATFORM_ID:Darwin>:-DHWM_OVER_XU=OFF>
+			$<$<PLATFORM_ID:Windows>:-DBUILD_WITH_STATIC_CRT=${STATIC_WINDOWS_RUNTIME}>
+	)
+	
+else()
 	ExternalProject_Add(
 		ext_librealsense
 		PREFIX librealsense
@@ -32,31 +58,6 @@ if (UNIX OR APPLE)
 			$<$<PLATFORM_ID:Darwin>:-DHWM_OVER_XU=OFF>
 			$<$<PLATFORM_ID:Windows>:-DBUILD_WITH_STATIC_CRT=${STATIC_WINDOWS_RUNTIME}>
 	)
-else()
-	ExternalProject_Add(
-		ext_librealsense
-		PREFIX librealsense
-		GIT_REPOSITORY https://github.com/IntelRealSense/librealsense.git
-		GIT_TAG v2.40.0 # 18 Nov 2020
-		UPDATE_COMMAND ""
-		CMAKE_ARGS
-			-D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-			-D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-			-D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-			-D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-			-D BUILD_SHARED_LIBS=OFF
-			-D BUILD_EXAMPLES=OFF
-			-D BUILD_UNIT_TESTS=OFF
-			-D BUILD_GLSL_EXTENSIONS=OFF
-			-D BUILD_GRAPHICAL_EXAMPLES=OFF
-			-D BUILD_PYTHON_BINDINGS=OFF
-			-D BUILD_WITH_CUDA=${BUILD_CUDA_MODULE}
-			-D FORCE_RSUSB_BACKEND=ON      # https://github.com/IntelRealSense/librealsense/wiki/Release-Notes#release-2400
-			-D USE_EXTERNAL_USB=ON
-			$<$<PLATFORM_ID:Darwin>:-DBUILD_WITH_OPENMP=OFF>
-			$<$<PLATFORM_ID:Darwin>:-DHWM_OVER_XU=OFF>
-			$<$<PLATFORM_ID:Windows>:-DBUILD_WITH_STATIC_CRT=${STATIC_WINDOWS_RUNTIME}>
-	)
 endif()
 
 
@@ -65,8 +66,12 @@ ExternalProject_Get_Property(ext_librealsense INSTALL_DIR)
 set(LIBREALSENSE_INCLUDE_DIR "${INSTALL_DIR}/include/") # "/" is critical.
 set(LIBREALSENSE_LIB_DIR "${INSTALL_DIR}/lib")
 
-#set(LIBREALSENSE_LIBRARIES realsense2 fw realsense-file usb) # The order is critical.
-set(LIBREALSENSE_LIBRARIES realsense2 realsense-file usb) # The order is critical.
+if (WIN32)
+	set(LIBREALSENSE_LIBRARIES realsense2 fw realsense-file usb) # The order is critical.
+else()
+	set(LIBREALSENSE_LIBRARIES realsense2 realsense-file usb) # The order is critical.
+endif()
+
 if(MSVC)    # Rename debug libs to ${LIBREALSENSE_LIBRARIES}. rem (comment) is no-op
     ExternalProject_Add_Step(ext_librealsense rename_debug_libs
         COMMAND $<IF:$<CONFIG:Debug>,rename,rem> realsense2d.lib realsense2.lib
