5f3933440be16606af4c9d7fdbae9ac58c1ba036
ludahai <ludahai19@163.com>
2024-10-14 14:18:11 +0800

    add ultra compressing support for packaging

--- scripts/sed_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -x "$(command -v sw_vers)" ]; then
-  echo 'MacOS sed call'
-  sed -i "" "s/..\/data\/4050\/lut_apa\/configs/.\/4050\/lut_apa\/configs/g" $1
-else
-  echo 'Linux sed call'
-  sed -i "s/..\/data\/4050\/lut_apa\/configs/.\/4050\/lut_apa\/configs/g" $1
-fi
