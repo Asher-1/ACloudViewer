e9903acebaa4476eaa2bfc5596d78ea6b6095ac6
ludahai <ludahai19@163.com>
2024-11-25 23:09:59 +0800

    fix ubuntu and macos github actions

--- docker/build-all-conda.sh
+++ docker/build-all-conda.sh
@@ -8,5 +8,6 @@ test -d docker || (
 )
 
 BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=20.04 docker/build-ubuntu.sh    \
-&& BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=22.04 docker/build-ubuntu.sh \
-&& BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh \
+&& BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=22.04 docker/build-ubuntu.sh
+# building with conda on ubuntu18.04 issues: '(1.0e+0 / 3.0e+0)' is not a constant expression
+# && BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh \
--- docker/build-release-conda.sh
+++ docker/build-release-conda.sh
@@ -7,4 +7,5 @@ test -d docker || (
 	exit 1
 )
 
-BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=18.04 docker/build-ubuntu.sh
+# building with conda on ubuntu18.04 issues: '(1.0e+0 / 3.0e+0)' is not a constant expression
+BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=20.04 docker/build-ubuntu.sh
