7900eae5a61f8ea914b1d07d4690e49b2b7c7309
ludahai <ludahai19@163.com>
2024-12-19 01:13:09 +0800

    fix linux missing qt plugins: styles

--- cmake/CMakeExternalLibs.cmake
+++ cmake/CMakeExternalLibs.cmake
@@ -92,10 +92,12 @@ message(STATUS "Qt5_BIN_DIR: " ${Qt5_BIN_DIR})
 message(STATUS "QT5_ROOT_PATH: " ${QT5_ROOT_PATH})
 message(STATUS "QT5_PLUGINS_PATH: " ${QT5_PLUGINS_PATH})
 set(QT5_PLUGINS_PATH_LIST   "${QT5_PLUGINS_PATH}/platforms")
-list(APPEND QT5_PLUGINS_PATH_LIST   "${QT5_PLUGINS_PATH}/platformthemes") # for useNativeDialogs support on linux
-list(APPEND QT5_PLUGINS_PATH_LIST   "${QT5_PLUGINS_PATH}/styles") # for useNativeDialogs support on windows
+if (WIN32)
+    list(APPEND QT5_PLUGINS_PATH_LIST   "${QT5_PLUGINS_PATH}/styles") # for useNativeDialogs support on windows
+endif()
 if (UNIX AND NOT APPLE)
     list(APPEND QT5_PLUGINS_PATH_LIST "${QT5_PLUGINS_PATH}/xcbglintegrations")
+    list(APPEND QT5_PLUGINS_PATH_LIST "${QT5_PLUGINS_PATH}/platformthemes") # for useNativeDialogs support on linux
 endif()
 list(APPEND QT5_PLUGINS_PATH_LIST "${QT5_PLUGINS_PATH}/iconengines")
 list(APPEND QT5_PLUGINS_PATH_LIST "${QT5_PLUGINS_PATH}/imageformats")
