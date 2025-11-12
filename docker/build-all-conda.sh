2456da86628e39a0e8fe489378bdacc604e6dfd9
ludahai <ludahai19@163.com>
2025-11-12 11:24:07 +0800

    rename docker scripts

--- docker/build-all-conda.sh
+++ docker/build-all-conda.sh
@@ -7,7 +7,8 @@ test -d docker || (
 	exit 1
 )
 
-BUILD_WITH_CONDA=ON CUDA_VERSION=12.6.3-cudnn UBUNTU_VERSION=20.04 docker/docker_build_local.sh    \
-&& BUILD_WITH_CONDA=ON CUDA_VERSION=12.6.3-cudnn UBUNTU_VERSION=22.04 docker/docker_build_local.sh
+BUILD_WITH_CONDA=ON CUDA_VERSION=12.6.3-cudnn UBUNTU_VERSION=20.04 docker/docker_build_local.sh \
+&& BUILD_WITH_CONDA=ON CUDA_VERSION=12.6.3-cudnn UBUNTU_VERSION=22.04 docker/docker_build_local.sh \
+&& BUILD_WITH_CONDA=ON CUDA_VERSION=12.6.3-cudnn UBUNTU_VERSION=24.04 docker/docker_build_local.sh
 # building with conda on ubuntu18.04 issues: '(1.0e+0 / 3.0e+0)' is not a constant expression
 # && BUILD_WITH_CONDA=ON CUDA_VERSION=11.8.0-cudnn8 UBUNTU_VERSION=18.04 docker/docker_build_local.sh \
